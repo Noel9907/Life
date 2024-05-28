ae8ad6c3c8a9d4f782e6fc4bedb0ff48617305e6
Noel <noelskocheekkaran@gmail.com>
2024-05-28 21:23:22 +0530

    3rd

--- life.js
+++ life.js
@@ -1,20 +1,24 @@
-let delayInMilliseconds=500;
+let delayInMilliseconds=1000;
 function position(){
-  for(let m=0;m<4;m++){
-    function name() {
-      //your code to be executed after 1 second
-         
-          for (let i = 1; i < 9; i++) {
-          let z = Math.floor((Math.random() * 100) + 1);
-          let t = Math.floor((Math.random() * 100) + 1);
-          document.getElementById("one"+i).style.top= z +"%";
-          document.getElementById("one"+i).style.left= t +"%";
-          document.getElementById("one"+i).style.display="block";
-          document.getElementById("one"+i).style.borderRadius="100%";
-          }
-      
-    }
-setTimeout(name(), delayInMilliseconds);
+ 
+    
+    
+setTimeout(function name(){
+  
+  
+    //your code to be executed after 1 second
+    for(var m=3;m<8;m++){
+        for (let i = 1; i < 9; i++) {
+        let z = Math.floor((Math.random() * 100) + 1);
+        let t = Math.floor((Math.random() * 100) + 1);
+        document.getElementById("one"+i).style.top= z +"%";
+        document.getElementById("one"+i).style.left= t +"%";
+        document.getElementById("one"+i).style.display="block";
+        document.getElementById("one"+i).style.borderRadius="100%";
+        
 }
+}
+}, delayInMilliseconds);
 
 }
+
